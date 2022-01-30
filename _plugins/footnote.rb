24688f47d64f5ae75215698ea19b101456452746
Ludovico Bessi <ludovicobessi@Ludovicos-MBP.Home>
2022-01-30 17:59:23 +0100

    disable: true

--- _plugins/footnote.rb
+++ _plugins/footnote.rb
@@ -13,3 +13,20 @@ module Jekyll
   end
   
 Liquid::Template.register_tag('fn', Jekyll::FootNoteTag)
+
+module JekyllFeed
+  class Generator < Jekyll::Generator
+    safe true
+    priority :lowest
+
+    alias_method :old_generate, :generate
+
+    def generate(site)
+      if site.config["feed"]["disable"]
+        Jekyll.logger.info "Jekyll Feed disabled"
+      else
+        old_generate(site)
+      end
+    end
+  end
+end
